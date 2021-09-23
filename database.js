76837924ff721fdf30e34bca4d15302179ae1a3c
Faadhil Ahamat <faadhil.aa@gmail.com>
2021-09-23 17:34:31 +0000

    complete user ranking db queries and set up routes

--- database.js
+++ database.js
@@ -1,14 +1,15 @@
-// module.exports = function() {
+// const { Pool } = require('pg');
+// const dbParams = require('./lib/db.js');
 
-  const { Pool } = require('pg');
-  const dbParams = require('./lib/db.js');
+// const pool = new Pool(dbParams);
+// pool.connect();
 
-  // Commenting out SQL database for now...
-  const pool = new Pool(dbParams);
-  pool.connect();
+// Get the poll using the user_link
+// @Params:
+// userLink = the user_link in the poll table.
 
 const getPollWithUserLink = function(userLink) {
-  return pool
+  return db
     .query(`SELECT *
     FROM poll_options
     JOIN poll on poll.id = poll_id
@@ -21,6 +22,56 @@ const getPollWithUserLink = function(userLink) {
       return null;
     });
 }
-return getPollWithUserLink;
+exports.getPollWithUserLink = getPollWithUserLink;
 
-// }
+
+// Adds the poll vote to user_answers.
+// @Params:
+// poll = object of the poll.
+// rankArray = array of integers containing the rank of each poll option.
+// voterName: name of the voter if the poll requires it.
+
+const addVote =  function(poll, rankArray, voterName) {
+  let queryString = '';
+  const queryParams = [];
+
+  for (let x = 0; x < rankArray; x++){
+
+    if (poll.name_req === true){
+    queryParams.push(voterName);
+
+    queryString += `
+    INSERT INTO user_answers (name, poll_option_id, rank)
+    VALUES ($${queryParams.length},`
+
+    queryParams.push(x);
+    queryString += `$${queryParams.length},`
+
+    queryParams.push(rankArray.length - rankArray[x]);
+    queryString += `$${queryParams.length})
+    RETURNING *`
+
+    } else {
+
+    queryParams.push(x);
+    queryString += `
+    INSERT INTO user_answers (poll_option_id, rank)
+    VALUES ($${queryParams.length},`
+
+    queryParams.push(rankArray.length - rankArray[x]);
+    queryString += `$${queryParams.length})
+    RETURNING *`
+    }
+  }
+
+  return db
+    .query(queryString, queryParams)
+    .then((result) => {
+       result.rows
+  })
+    .catch((err) => {
+      return null;
+    });
+}
+exports.addVote = addVote;
+
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const database = require('./database');
+
 // load .env data into process.env
 require('dotenv').config();
 
@@ -43,13 +45,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const userRankingRoutes = require("./routes/userRanking");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
-
+app.use("/userranking", userRankingRoutes(db));
 
 // Home page
 // Warning: avoid creating more routes in this file!
